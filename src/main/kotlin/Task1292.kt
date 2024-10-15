d5aa6cfc1a34ffe51e98a58afe57400b4353decb
Kateryna <kateryna.babachanakh1@etu.univ-lorraine.fr>
2024-10-15 22:15:58 +0200

    Adding solution for task 1292 (error WA - attempt 2)

--- src/main/kotlin/Task1292.kt
+++ src/main/kotlin/Task1292.kt
@@ -1,11 +1,20 @@
+import kotlin.math.ceil
+import kotlin.math.log2
+
 /**
  * Task with ID 1292
  */
 fun main() {
     val n = readLine()?.toIntOrNull() ?: return
 
-    val minTime = n.toString(2).length - 1
-    val maxTime = n - Integer.bitCount(n)
+    val maxTime = ceil(log2(n.toDouble())).toInt()
+
+    var minTime = 0
+    var remaining = n
+    while (remaining > 1) {
+        remaining /= 2
+        minTime++
+    }
 
     println("$minTime $maxTime")
 }
