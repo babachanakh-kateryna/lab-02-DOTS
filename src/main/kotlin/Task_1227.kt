d7215c2c0654e801ff917183004527ddb8622000
Kateryna <kateryna.babachanakh1@etu.univ-lorraine.fr>
2024-10-15 12:11:01 +0200

    Adding the solution for task 1227 (incorrect)

--- src/main/kotlin/Task_1227.kt
+++ src/main/kotlin/Task_1227.kt
@@ -1,12 +1,19 @@
 package org.example
 
+import kotlin.math.sqrt
+
 fun main() {
     val n = readLine()?.toIntOrNull() ?: return
+    val sqrtN = sqrt(n.toDouble()).toInt()
 
-    for (d in (n / 2) downTo 1) {
+    for (d in sqrtN downTo 1) {
         if (n % d == 0) {
-            println(d)
-            break
+            if (d != n) {
+                println(n / d)
+            } else {
+                println(d)
+            }
+            return
         }
     }
 }
