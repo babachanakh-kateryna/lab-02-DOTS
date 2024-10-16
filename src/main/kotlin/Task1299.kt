5fa2fffbed8929258ea281c434a0745a16a01dce
Kateryna <kateryna.babachanakh1@etu.univ-lorraine.fr>
2024-10-16 16:04:24 +0200

    Adding solution for task 1299 (error WA - attempt 2)

--- src/main/kotlin/Task1299.kt
+++ src/main/kotlin/Task1299.kt
@@ -1,14 +1,22 @@
-
 import java.math.BigInteger
 
 fun countNumbers(M: Int, N: Int): BigInteger {
     val lower = BigInteger.TEN.pow(M - 1)  // 10^(M-1)
     val upper = BigInteger.TEN.pow(N)      // 10^N
-    return upper.subtract(lower)
+    return upper - lower
 }
 
 fun main() {
-    val input = readLine()!!
-    val (M, N) = input.split(" ").map { it.toInt() }
+    val inputs = mutableListOf<Int>()
+    while (inputs.size < 2) {
+        val input = readLine()!!.trim()
+        if (input.contains(" ")) {
+            inputs.addAll(input.split(" ").map { it.toInt() })
+        } else {
+            inputs.add(input.toInt())
+        }
+    }
+
+    val (M, N) = inputs
     println(countNumbers(M, N))
 }
